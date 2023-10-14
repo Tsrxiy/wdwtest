f71c37d0f89fd0043b225e5aa264072aa523d9fe
Tsrxiy <142523451+Tsrxiy@users.noreply.github.com>
2023-10-14 17:09:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var passowrd = "exileisgood!";
+
+function passcheck() {
+if(document.getElementById('pass1').value != passowrd) {
+    alert('Wrong Passowrd, Try Again.');
+    return false;
+}
+
+if(document.getElementById('pass1').value != passowrd) {
+    alert('Correct Passowrd, Hit OK to enter webpage.');
+}
+}
